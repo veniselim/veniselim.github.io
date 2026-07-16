885920971f3176b36458f473f244e9e16cc23eda
ven <251218B@mymail.nyp.edu.sg>
2026-07-16 14:06:39 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+/*Full compatibility for all browsers + Full screen on Desktop*/
+<button id="btnFS">FullScreen</button>
+<button id="btnWS">Exit Full Screen</button>
+const btnFS=document.querySelector("#btnFS");
+const btnWS=document.querySelector("#btnWS");
+btnFS.addEventListener("click",enterFullscreen);
+btnWS.addEventListener("click",exitFullscreen);
+function enterFullscreen() { //must be called by user generated event
+if (document.documentElement.requestFullscreen) {
+document.documentElement.requestFullscreen();
+} else if (document.documentElement.mozRequestFullScreen) { // Firefox
+document.documentElement.mozRequestFullScreen();
+} else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+document.documentElement.webkitRequestFullscreen();
+} else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+document.documentElement.msRequestFullscreen();
+}
+}
+function exitFullscreen() {
+if (document.exitFullscreen) {
+document.exitFullscreen();
+} else if (document.mozCancelFullScreen) { // Firefox
+document.mozCancelFullScreen();
+} else if (document.webkitExitFullscreen) { // Chrome, Safari, and Opera
+document.webkitExitFullscreen();
+} else if (document.msExitFullscreen) { // IE/Edge
+document.msExitFullscreen();
+}
+}
+/* Report Window Size */
+const heightOutput = document.querySelector("#height");
+const widthOutput = document.querySelector("#width");
+function reportWindowSize() {
+heightOutput.textContent = window.innerHeight;
+widthOutput.textContent = window.innerWidth;
+}
+reportWindowSize();
+window.addEventListener("resize",reportWindowSize);//when resize, update
+report
